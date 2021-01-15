d10c3649c10853f7cbf5e3587de992e0e96ba59e
Haseeb Abid <haseeb.abid5@gmail.com>
2021-01-15 00:59:41 -0500

    Fixed Graphics, Materials, Monster form 2
    fixed graphcal issues and created custom materials. Also added the next evolution to the first monster

--- Assets/PlayerFollow.cs
+++ Assets/PlayerFollow.cs
@@ -6,25 +6,34 @@ public class PlayerFollow : MonoBehaviour
 {
 
     public GameObject player;
-    public float range = 5.0f, speed = 2.0f, space = 2.0f;
+    public float range = 5.0f, speed = 2.0f, space = 2.0f, initSpeed = 0f, boost = 1f, buffer;
     public Vector3 offset;
 
     private Vector3 velocity = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-     
         if(Mathf.Abs(Vector3.Distance(transform.position, player.transform.position)) > range)
         {
             transform.LookAt(player.transform);
             transform.position = Vector3.SmoothDamp(transform.position, player.transform.position, ref velocity, speed);
             GetComponent<Animator>().SetBool("isWalking", true);
+            if(Mathf.Abs(Vector3.Distance(transform.position, player.transform.position)) > (range + buffer))
+            {
+                GetComponent<Animator>().SetBool("Roll", true);
+                speed = initSpeed - boost;
+            }
+            else
+            {
+                GetComponent<Animator>().SetBool("Roll", false);
+                speed = initSpeed;
+            }
 
         }
         else { 
